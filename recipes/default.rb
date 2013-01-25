6f7eddd44ffbabc6420bb42b94df80cb8ddca8d6
Umang Chouhan <uchouhan@optimiscorp.com>
2013-01-25 12:39:20 -0800

    Fixing resource name.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@ raise 'No vhosts configured for this node.' unless node['apache2']['vhosts']
 include_recipe 'apache2'
 
 node['apache2']['vhosts'].each do |vhost|
-  web_app application['name'] do
+  web_app vhost['name'] do
     template 'web_app.ssl.conf.erb'
     server_name vhost['name']
     server_aliases vhost['aliases']
