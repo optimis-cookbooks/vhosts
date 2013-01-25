c602cd13c67fb6c4058fd7bfaa3b9292fded9b73
Umang Chouhan <uchouhan@optimiscorp.com>
2013-01-25 08:57:00 -0800

    Using a local variable to access the application.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,9 @@ raise 'No vhosts configured for this node.' unless node['apache2']['vhosts']
 
 include_recipe 'apache2'
 
-node['apache2']['vhosts'].each do |application|
+node['apache2']['vhosts'].each do |vhost|
+  application = vhost
+
   web_app application['name'] do
     template 'web_app.ssl.conf.erb'
     server_name application['name']
