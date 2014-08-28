2c6de29c9c45d5af27d95f2fe04693a8a5cb3665
Umang Chouhan <uchouhan@optimiscorp.com>
2014-08-28 20:04:36 +0530

    Adding docker integration test.
    [References #77805654]

--- test/kitchen/cookbooks/vhosts_test/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-override['apache2']['vhosts'] = [
-  { 
-    "name" => 'test.optimispt.com',
-    "title" => 'Test',
-    "domain" => 'optimispt',
-    "aliases" => [ 'content-staging.optimispt.com' ],
-    "environment" => 'production'
-  }
-]
